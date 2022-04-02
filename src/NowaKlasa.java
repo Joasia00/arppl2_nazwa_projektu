f1de103446f4112485206d7e11f0cf62e423d5e3
Joasia00 <Asiawegiel123@wp.pl>
2022-04-02 12:24:33 +0200

    Drugi wpis w historii plików

---
+++ src/NowaKlasa.java
@@ -0,0 +1,13 @@
+/* komentarz wieloliniowy */
+
+/**
+ * Ta klasa jest moją pierwszą klasą!
+ * Ten komenatrz jest częścią dokumentacji!
+ */
+public class NowaKlasa {
+   // Do zrobienia
+    //TODO: musze pamietac zeby kupic mleko
+    // fixme: koło odpadło z samochodu
+
+    // Kod powinien się sam dokumentować
+}
